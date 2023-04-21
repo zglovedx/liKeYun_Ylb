2835fe5fc2f6c2ef24784610582356a520a093bb
coffee <60502331+zglovedx@users.noreply.github.com>
2023-04-21 19:19:49 +0800

    Add files via upload

---
+++ console/Db.php
@@ -0,0 +1,16 @@
+<?php
+
+// 数据库操作类
+include 'DbClass.php';
+
+// 数据库配置
+$config = [
+    'db_host' => 'sql.freedb.tech:3306',
+    'db_port' => 3306,
+    'db_name' => 'freedb_zglovedx',
+    'db_user' => 'freedb_zglovedx',
+    'db_pass' => 'GhCH9#t@xfRSek3',
+    'folderNum' => '1',
+    'db_prefix' => ''
+];
+?>
